ed8406c4be3b2ea7b1d5ffb587f2b40b7533ab71
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2026-02-27 11:49:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "21655449"))
 API_HASH = os.environ.get("API_HASH", "112be9974e163f6dbd645ce4b94f4e6a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7850690171:AAGeDrbeXp8rrno3pknQA3XRNWCIZTkIWcQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7812581699:AAF5Avwz02if_Jhaci25LzfjVB9Fk7LWz7g")
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://kailash:pass@cluster0.sqtztxm.mongodb.net/?retryWrites=true&w=majority")
 # 👇 Multiple admins support
 ADMINS = list(map(int, os.environ.get("ADMINS", "1229852181").split()))

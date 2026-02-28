2777e8951204955d437ff3a89e83aa691d2f00fe
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2026-02-28 11:13:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ API_HASH = os.environ.get("API_HASH", "112be9974e163f6dbd645ce4b94f4e6a")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7812581699:AAE914UAENlDmOWpKjpqSOj65avld8Pxww8")
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://kailash:pass@cluster0.sqtztxm.mongodb.net/?retryWrites=true&w=majority")
 # 👇 Multiple admins support
-ADMINS = list(map(int, os.environ.get("ADMINS", "1229852181").split()))
+ADMIN_IDS = [1229852181]  # apna Telegram user ID daalo
